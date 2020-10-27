b807516e6dec49e73eb10456512b3773699e036d
unknown <jessop.lucas@gmail.com>
2020-10-27 16:20:31 -0600

    finished chat

---
+++ server/index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const mc = require('./controller/messages_controller')
+const app = express()
+
+app.use(express.json())
+
+
+app.post('/api/messages', mc.create)
+app.get('/api/messages', mc.read)
+app.put('/api/messages/:id', mc.update)
+app.delete('/api/messages/:id', mc.delete)
+
+const port = 3001
+app.listen(port, () =>{
+    console.log(`Server listening on port ${port}`)
+})
